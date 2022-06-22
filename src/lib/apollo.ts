13cc67d5d68782e0087a791f88340ec3cbc014eb
Rodrigo Reis <rodrigo_reis_87@hotmail.com>
2022-06-21 21:16:31 -0300

    Aula 02

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-    uri: 'https://',
-    cash = new InMemoryCache();
+    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4oiyg6q07q701xxe7l89lhc/master',
+    cache: new InMemoryCache()
 })
