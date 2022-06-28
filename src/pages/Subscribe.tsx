a36e2d7e237870388ce14240450db8a627f6a0ef
Rodrigo Reis <rodrigo_reis_87@hotmail.com>
2022-06-28 20:11:34 -0300

    Responsive-test

--- src/pages/Subscribe.tsx
+++ src/pages/Subscribe.tsx
@@ -28,15 +28,20 @@ export function Subscribe() {
 
     return (
         <div className="min-h-screen bg-blur bg-cover bg-no-repeat flex flex-col items-center">
-            <div className="w-full max-w-[1100px] flex items-center justify-between mt-20 mx-auto">
-                <div className="max-w-[640px]">
+            <div className="w-full items-center justify-between mt-20 mx-auto 
+            tablet:inline tablet:max-w-[460px] 
+            laptop:flex laptop:max-w-[844px]
+            desktop:flex desktop:max-w-[1100px]">
+                <div className="tablet:max-w-[640px] 
+                laptop:max-w-[1024px] 
+                desktop:max-w-[1280px]">
                     <Logo />
 
-                    <h1 className="mt-8 text-[2.5rem] leading-tight">
+                    <h1 className="mt-8 tablet:text-[2.5rem] leading-tight tablet:text-center laptop:text-left desktop:text-left">
                         Construa uma <strong className="text-blue-500">aplicação completa</strong> , do zero, com <strong className="text-blue-500">React</strong>
                     </h1>
 
-                    <p className="mt-4 text-gray-200 leading-relaxed">
+                    <p className="mt-4 text-gray-200 leading-relaxed tablet:text-center laptop:text-left desktop:text-left">
                         Em apenas uma semana você vai dominar na prática uma das tecnologias mais utilizadas e com alta demanda para acessar as melhores oportunidades do mercado.
                     </p>
                 </div>
